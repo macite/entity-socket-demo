cdf8e53efc61b8dabee02cc00eb774097e6a0c85
Andrew Cain <macite@gmail.com>
2021-08-09 12:02:38 +1000

    chore: add rack-cors to enable ajax access

--- certain-ice-api/config/application.rb
+++ certain-ice-api/config/application.rb
@@ -18,5 +18,13 @@ module Api
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # CORS config
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i(get post put delete options)
+      end
+    end
   end
 end
