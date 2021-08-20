d40ef53206d0dd53b7238e99a911599c737bf1f0
Andrew Cain <macite@gmail.com>
2021-08-20 22:55:09 +1000

    fix: update entity service code to work with npm package

--- certain-ice-ui/src/app/model/user.service.ts
+++ certain-ice-ui/src/app/model/user.service.ts
@@ -2,6 +2,7 @@ import { User } from './user';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EntityService } from '@doubtfire-lms/entity-service';
+import API_URL from './apiURL';
 
 @Injectable()
 export class UserService extends EntityService<User> {
@@ -11,7 +12,7 @@ export class UserService extends EntityService<User> {
   constructor(
     httpClient: HttpClient,
   ) {
-    super(httpClient);
+    super(httpClient, API_URL);
   }
 
   protected createInstanceFrom(json: any, other?: any): User {
