d80d4ba36101d6b2e39a37fa6ff88b8e07156f4d
Andrew Cain <macite@gmail.com>
2021-11-26 21:38:43 +1100

    fix: remove user identification in connection

--- certain-ice-api/app/channels/application_cable/connection.rb
+++ certain-ice-api/app/channels/application_cable/connection.rb
@@ -1,22 +1,25 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-  identified_by : current_user
+  # identified_by : current_user
   
   def connect
-    self.current_user = find_verified_user
+    puts 'connect'
+    # self.current_user = find_verified_user
+    ActionCable.server.broadcast("chat_channel", {type:'alert', data:"connected"})
   end
 
   def disconnect
-    ActionCable.server.broadcast("web_notification_channel", type:'alert', data:"#{current_user} disconnected")
+    puts 'disconnect'
+    ActionCable.server.broadcast("chat_channel", {type:'alert', data:"disconnected"})
   end
 
-  private
-    def find_verified_user
-      verified_user == env['warden'].user
-      verified_user
-      else
-        reject_unauthorized_connection
-      end
-    end
+  # private
+  #   def find_verified_user
+  #     verified_user == env['warden'].user
+  #     verified_user
+  #     else
+  #       reject_unauthorized_connection
+  #     end
+  #   end
   end
 end
